b1265a08fe1513b3c6b81471c6b98c753fd586de
worku005 <dirk-jan.vanworkum@wur.nl>
2025-02-17 16:18:10 +0100

    update merge tsv file to sort columns more reasonably

--- workflow/scripts/merge_tsv_files.sh
+++ workflow/scripts/merge_tsv_files.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
+# Written by Dirk-Jan van Workum (2025) with help of claude.ai
 
-# First pass to get the headers
-headers=$(head -n1 $@ | awk 'NR%3==2' | tr '\t' '\n' | sort -u | tr '\n' '\t' | sed 's/\t$/\n/')
+# First pass to get all headers and identify common columns (starting with 'Name' if exists)
+headers=$(awk '
+BEGIN {
+    FS=OFS="\t";
+    delete col_map;
+    total = 0;
+}
+
+FNR==1 {
+    total++;
+    for (i=1;i<=NF;i++) {
+        if (!($i in col_map)) col_map[$i] = 1;
+        else col_map[$i]++;
+    }
+}
+
+END {
+    if ("Name" in col_map) { # If Name column exists, print it first
+        print "Name";
+    }
+    for (col in col_map) if (col_map[col] == total && col != "Name") { # Print common columns
+        print col;
+    }
+    for (col in col_map) if (col_map[col] != total && col != "Name") { # Print non-common columns
+        print col;
+    }
+}
+' $@ | tr '\n' '\t' | sed 's/\t$//')
 
 # Second pass to print the headers and the lines in the correct order
 awk -v headers="$headers" '
