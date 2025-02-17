f5d8d35302ca1d01bf7fb8bb527cda204047a54f
worku005 <dirk-jan.vanworkum@wur.nl>
2025-02-17 16:25:51 +0100

    just hardcode the column names' order

--- workflow/scripts/merge_tsv_files.sh
+++ workflow/scripts/merge_tsv_files.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 # Written by Dirk-Jan van Workum (2025) with help of claude.ai
 
-# First pass to get all headers and identify common columns (starting with 'Name' if exists)
+# First pass to get all relevant headers if available (cfr. 13.statistics.smk)
 headers=$(awk '
 BEGIN {
     FS=OFS="\t";
     delete col_map;
-    total = 0;
 }
 
 FNR==1 {
-    total++;
-    for (i=1;i<=NF;i++) {
-        if (!($i in col_map)) col_map[$i] = 1;
-        else col_map[$i]++;
-    }
+    for (i=1;i<=NF;i++) if (!($i in col_map)) col_map[$i] = 1;
 }
 
 END {
-    if ("Name" in col_map) { # If Name column exists, print it first
-        print "Name";
-    }
-    for (col in col_map) if (col_map[col] == total && col != "Name") { # Print common columns
-        print col;
-    }
-    for (col in col_map) if (col_map[col] != total && col != "Name") { # Print non-common columns
-        print col;
-    }
+    #printf "Name\\tTotal length\\t#sequences\\tN50\\t#genes (full)\\t#genes (coding)\\t#transcripts (coding)\\t#chromosomes\\tTotal length (chromosomes)\\t#unassigned sequences\\tTotal length (unassigned sequences)\\tTotal QV\\tK-mer completeness\\t#contigs\\tContig N50\\tInput data\\tAssembler\\n" > {output.tsv}
+    if ("Name" in col_map) print "Name";
+    if ("Total length" in col_map) print "Total length";
+    if ("#sequences" in col_map) print "#sequences";
+    if ("N50" in col_map) print "N50";
+    if ("#genes (full)" in col_map) print "#genes (full)";
+    if ("#genes (coding)" in col_map) print "#genes (coding)";
+    if ("#transcripts (coding)" in col_map) print "#transcripts (coding)";
+    if ("#chromosomes" in col_map) print "#chromosomes";
+    if ("Total length (chromosomes)" in col_map) print "Total length (chromosomes)";
+    if ("#unassigned sequences" in col_map) print "#unassigned sequences";
+    if ("Total length (unassigned sequences)" in col_map) print "Total length (unassigned sequences)";
+    if ("Total QV" in col_map) print "Total QV";
+    if ("K-mer completeness" in col_map) print "K-mer completeness";
+    if ("#contigs" in col_map) print "#contigs";
+    if ("Contig N50" in col_map) print "Contig N50";
+    if ("Input data" in col_map) print "Input data";
+    if ("Assembler" in col_map) print "Assembler";
 }
 ' $@ | tr '\n' '\t' | sed 's/\t$//')
 
