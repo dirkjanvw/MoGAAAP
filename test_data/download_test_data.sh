d518fbc1afc77e79cc36623e8c4167483d05d2b4
worku005 <dirk-jan.vanworkum@wur.nl>
2025-02-20 15:26:37 +0100

    fix links

--- test_data/download_test_data.sh
+++ test_data/download_test_data.sh
@@ -9,9 +9,9 @@ fi
 cd test_data/
 
 # Download the test data
-wget "ftp://download.big.ac.cn/gsa2/CRA008584/CRR591691/CRR591691.fastq.gz" #43_elk_1
-wget "ftp://download.big.ac.cn/gsa2/CRA008584/CRR591692/CRR591692.fastq.gz" #44_ket_10
-wget "ftp://download.big.ac.cn/gsa2/CRA008584/CRR591693/CRR591693.fastq.gz" #45_meh_0
+wget "ftp://download.big.ac.cn/gsa2/CRA008584/CRR591691/CRR591691.fq.gz" #43_elk_1
+wget "ftp://download.big.ac.cn/gsa2/CRA008584/CRR591692/CRR591692.fq.gz" #44_ket_10
+wget "ftp://download.big.ac.cn/gsa2/CRA008584/CRR591693/CRR591693.fq.gz" #45_meh_0
 wget "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/735/GCF_000001735.4_TAIR10.1/GCF_000001735.4_TAIR10.1_genomic.fna.gz" #Arabidopsis thaliana Col-0 genome assembly
 wget -O Araport11_GFF3_genes_transposons.current.gff.gz "https://www.arabidopsis.org/api/download-files/download?filePath=Genes/Araport11_genome_release/Araport11_GFF3_genes_transposons.current.gff.gz" #Arabidopsis thaliana Col-0 genome annotation
 wget -O mitochondrion.fasta "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nuccore&id=NC_037304.1&report=fasta&format=text" #Arabidopsis thaliana Col-0 mitochondrion genome
